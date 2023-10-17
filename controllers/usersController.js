2b722818f5aa3ef751a12d54961b8ad22cbf03fe
Edgineer10 <redlilan15@gmail.com>
2023-10-17 15:59:20 +0900

    fix

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -3,7 +3,6 @@ const Note = require('../models/Note')
 const asyncHandler = require('express-async-handler')
 const bcrypt = require('bcrypt')
 
-
 // @desc Get all users
 // @route GET /users
 // @access Private
